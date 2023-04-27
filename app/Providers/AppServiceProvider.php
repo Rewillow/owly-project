815385010c127497d1f6c251cd4e59a79a13ec85
Samuele <samuele.gugliotta23@gmail.com>
2023-04-27 12:34:31 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
-        URL::forceScheme('https');
+        
+        if($this->app->environment('production') {
+            URL::forceScheme('https');
+        });
     }
 }
